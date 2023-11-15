45c6aeb6f7bdc99029d26cb786325694843a1af2
Mohamed <alxmohamedemadeldin@gmail.com>
2023-11-14 22:59:44 -0800

    string updated

---
+++ string_utils.c
@@ -0,0 +1,99 @@
+#include "shell.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @str: A pointer to the string.
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+	if (s[0] == '\0')
+		return (0);
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * _strcmp - Compare two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ * Return: 0 if the strings are equal, a non-zero value otherwise.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; ((s1[i] != '\0') || (s2[i] != '\0')); i++)
+	{
+		if (s1[i] - s2[i] != 0)
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+/**
+ * _strdup - creates an array of chars
+ * @str: array size
+ * Return: array refrance or NULL
+ */
+char *_strdup(char *str)
+{
+	int len;
+	char *arr;
+
+	if (str == NULL)
+		return (NULL);
+	len = _strlen(str);
+	arr = malloc((sizeof(char) * len) + 1);
+	if (arr == NULL)
+		return (NULL);
+	arr[len] = '\0';
+	while (len--)
+		arr[len] = str[len];
+	return (arr);
+}
+
+/**
+ * _strcpy - Copy one string to another.
+ * @dest: The destination string.
+ * @src: The source string.
+ * Return: A pointer to the destination string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strcat - Concatenate two strings.
+ * @dest: The destination string.
+ * @src: The source string to append.
+ * Return: A pointer to the concatenated string.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	len = _strlen(dest);
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[len + i] = src[i];
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
+
